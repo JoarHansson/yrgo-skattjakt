546fff62532b233f227ce787fa875d7592d0890c
wlanhage <wlanhage@gmail.com>
2024-08-22 09:27:03 +0200

    Working image on message popup

--- src/components/messagePopup.tsx
+++ src/components/messagePopup.tsx
@@ -1,28 +1,50 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Button from "@/content/button_ok.png";
+import Scroll from "@/content/scroll.png";
+import Pirat from "@/content/avatarer/man1.png";
+
+import Monkey from "@/content/djur/monkey.png";
+import Croc from "@/content/djur/croc.png";
+import Seal from "@/content/djur/seal.png";
+import Parrot from "@/content/djur/parrot.png";
+
+const imageMap = {
+  Monkey: Monkey,
+  Croc: Croc,
+  Seal: Seal,
+  Parrot: Parrot,
+};
 
 interface MessagePopupProps {
   description: string;
   name: string;
-  image: string;
+  characterImage: string;
   onClose: () => void;
 }
 
-export default function messagePopup({
+export default function MessagePopup({
   description,
   name,
-  image,
+  characterImage,
   onClose,
 }: MessagePopupProps) {
+  // Use type assertion and access the `src` property of the StaticImageData
+  const characterImgSrc = imageMap[characterImage as keyof typeof imageMap].src;
+
   return (
-    <div className="w-screen h-screen bg-white flex justify-center items-center">
-      <div className="w-5/6 h-4/6 bg-white border-black border-8 rounded-xl p-8 flex flex-col justify-around items-center">
+    <div className="w-screen h-screen bg-cyan-300 flex justify-center items-center">
+      <img src={Scroll.src} alt="" className="absolute top-24" />
+      <img
+        src={characterImgSrc}
+        width={140}
+        className="absolute top-28 left-8"
+      />
+      <div className="w-5/6 h-4/6 bg-orange-200 rounded-2xl p-8 flex flex-col justify-around items-center">
         <div>
-          <img className="rounded-xl" src={image} alt="Karaktär" />
           <h2>{name}</h2>
         </div>
         <ScrollArea>{description}</ScrollArea>
-        <img src={Button.src} onClick={onClose} className=""></img>
+        <img src={Button.src} onClick={onClose} className="" />
       </div>
     </div>
   );
