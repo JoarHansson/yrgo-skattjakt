c7c8b3ee63400bea0cd784fc863c5c7c2b76c994
wlanhage <wlanhage@gmail.com>
2024-08-22 15:47:10 +0200

    Working multiple clicks on OK button and random mission logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import UserSettingsProvider from "./user-settings-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <UserSettingsProvider>{children}</UserSettingsProvider>
+        <Toaster />
       </body>
     </html>
   );
--- src/components/map-component.tsx
+++ src/components/map-component.tsx
@@ -10,13 +10,29 @@ import QuestionMark from "./svg/question-mark";
 import MessagePopup from "./messagePopup";
 import Header from "./header";
 import missionData from "../missions.json";
+import MissionPopup from "./missionPopup";
+import { useToast } from "@/components/ui/use-toast";
 
 interface MessagePopupProps {
+  congratsMessage: string;
   description: string;
+  storyline: string;
   name: string;
   characterImage: string;
   icon: string;
   onClose: () => void;
+}
+
+interface PopupContentProps extends MessagePopupProps {
+  type: "marker" | "mission";
+}
+
+function shuffleArray(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 function MapComponent() {
@@ -24,23 +40,17 @@ function MapComponent() {
     longitude: number;
     latitude: number;
   } | null>(null);
-
-  useEffect(() => {
-    console.log("Mission Data:", missionData);
-  }, []);
-
   const [goldCounter, setGoldCounter] = useState(0);
   const [finishedMarkers, setFinishedMarkers] = useState<number[]>([]);
   const [finishedMissions, setFinishedMissions] = useState<number[]>([]);
-
   const [clickableMissions, setClickableMissions] = useState<number[]>([]);
   const [clickableMarkers, setClickableMarkers] = useState<number[]>([]);
   const geoControlRef = useRef<mapboxgl.GeolocateControl>(null);
   const markerRef = useRef<mapboxgl.Marker>(null);
   const missionRef = useRef<mapboxgl.Marker>(null);
-
+  const [missionDataState, setMissionData] = useState(missionData);
   const [popupVisible, setPopupVisible] = useState(false);
-  const [popupContent, setPopupContent] = useState<MessagePopupProps | null>(
+  const [popupContent, setPopupContent] = useState<PopupContentProps | null>(
     null
   );
 
@@ -54,13 +64,30 @@ function MapComponent() {
       const lat = e.coords.latitude;
       const position = [lon, lat];
       console.log(position);
-
       setUserLocation({
         longitude: e.coords.longitude,
         latitude: e.coords.latitude,
       });
     });
   };
+
+  useEffect(() => {
+    const shuffledMissions = shuffleArray([...missionData]);
+    const newMissions = missionData.map((mission, index) => ({
+      ...mission,
+      congratsMessage: shuffledMissions[index].congratsMessage,
+      description: shuffledMissions[index].description,
+      storyline: shuffledMissions[index].storyline,
+      name: shuffledMissions[index].name,
+      characterImage: shuffledMissions[index].characterImage,
+      icon: shuffledMissions[index].icon,
+    }));
+    setMissionData(newMissions);
+  }, []);
+
+  useEffect(() => {
+    console.log("Mission Data:", missionDataState);
+  }, [missionDataState]);
 
   useEffect(() => {
     console.log(`Gold Counter: ${goldCounter}`);
@@ -80,7 +107,7 @@ function MapComponent() {
         .filter((id) => id !== -1);
       setClickableMarkers(newClickableMarkers);
 
-      const newClickableMissions = missionData
+      const newClickableMissions = missionDataState
         .map((mission) => {
           const from = point([userLocation.longitude, userLocation.latitude]);
           const to = point([mission.longitude, mission.latitude]);
@@ -96,7 +123,7 @@ function MapComponent() {
   useEffect(() => {
     console.log("Clickable Missions:", clickableMissions);
     console.log("Clickable markers:", clickableMarkers);
-  }, [clickableMissions]);
+  }, [clickableMissions, clickableMarkers]);
 
   const handleMarkerClick = (markerId: number) => {
     if (!finishedMarkers.includes(markerId)) {
@@ -116,6 +143,8 @@ function MapComponent() {
     }
   };
 
+  const { toast } = useToast();
+
   return (
     <>
       <div
@@ -128,8 +157,6 @@ function MapComponent() {
         }}
       >
         <Header goldCounter={goldCounter} />
-        {/* lon: {userLocation?.longitude} <br />
-        lat: {userLocation?.latitude} */}
       </div>
       <Map
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
@@ -148,7 +175,6 @@ function MapComponent() {
           onGeolocate={() => getUserCoordinates()}
           fitBoundsOptions={{ maxZoom: 17 }}
         />
-
         {markersData.map((marker) => (
           <div className="z-30" key={marker.id}>
             <Marker
@@ -162,11 +188,12 @@ function MapComponent() {
                   className="stroke-green-500 scale-150 z-50"
                   onClick={() => {
                     console.log(`${marker.name} clicked`);
-                    alert("already completed.");
+                    toast({
+                      description: "Denna plats har du redan utforskat!",
+                    });
                   }}
                 />
               )}
-
               {clickableMarkers.includes(marker.id) &&
                 !finishedMarkers.includes(marker.id) && (
                   <QuestionMark
@@ -174,9 +201,10 @@ function MapComponent() {
                     onClick={() => {
                       console.log(`${marker.name} clicked`);
                       setPopupVisible(true);
-
                       setPopupContent({
                         description: marker.description,
+                        storyline: marker.storyline,
+                        congratsMessage: marker.congratsMessage,
                         name: marker.name,
                         characterImage: marker.image,
                         icon: marker.icon,
@@ -185,26 +213,27 @@ function MapComponent() {
                           handleMarkerClick(marker.id);
                           incrementGoldCounter();
                         },
+                        type: "marker",
                       });
                     }}
                   />
                 )}
-
               {!clickableMarkers.includes(marker.id) &&
                 !finishedMarkers.includes(marker.id) && (
                   <QuestionMark
                     className="stroke-slate-50 scale-150 z-50"
                     onClick={() => {
                       console.log(`${marker.name} clicked`);
-                      alert("access denied. come closer.");
+                      toast({
+                        description: "Du måste komma närmare!",
+                      });
                     }}
                   />
                 )}
             </Marker>
           </div>
         ))}
-
-        {missionData.map((mission) => (
+        {missionDataState.map((mission) => (
           <div className="z-30" key={mission.id}>
             <Marker
               longitude={mission.longitude}
@@ -217,11 +246,12 @@ function MapComponent() {
                   className="stroke-green-500 scale-150 z-50"
                   onClick={() => {
                     console.log(`${mission.name} clicked`);
-                    alert("already completed.");
+                    toast({
+                      description: "Denna plats har du redan utforskat!",
+                    });
                   }}
                 />
               )}
-
               {clickableMissions.includes(mission.id) &&
                 !finishedMissions.includes(mission.id) && (
                   <QuestionMark
@@ -230,7 +260,9 @@ function MapComponent() {
                       console.log(`${mission.name} clicked`);
                       setPopupVisible(true);
                       setPopupContent({
+                        congratsMessage: mission.congratsMessage,
                         description: mission.description,
+                        storyline: mission.storyline,
                         name: mission.name,
                         characterImage: mission.image,
                         icon: mission.icon,
@@ -239,18 +271,20 @@ function MapComponent() {
                           handleMissionClick(mission.id);
                           incrementGoldCounter();
                         },
+                        type: "mission",
                       });
                     }}
                   />
                 )}
-
               {!clickableMissions.includes(mission.id) &&
                 !finishedMissions.includes(mission.id) && (
                   <QuestionMark
                     className="stroke-slate-50 scale-150 z-50"
                     onClick={() => {
                       console.log(`${mission.name} clicked`);
-                      alert("access denied. come closer.");
+                      toast({
+                        description: "Du måste komma närmare!",
+                      });
                     }}
                   />
                 )}
@@ -258,7 +292,6 @@ function MapComponent() {
           </div>
         ))}
       </Map>
-
       {popupVisible && popupContent && (
         <div
           style={{
@@ -269,13 +302,27 @@ function MapComponent() {
             zIndex: 200,
           }}
         >
-          <MessagePopup
-            description={popupContent.description}
-            name={popupContent.name}
-            characterImage={popupContent.characterImage}
-            icon={popupContent.icon}
-            onClose={popupContent.onClose}
-          />
+          {popupContent.type === "marker" ? (
+            <MessagePopup
+              congratsMessage={popupContent.congratsMessage}
+              storyline={popupContent.storyline}
+              description={popupContent.description}
+              name={popupContent.name}
+              characterImage={popupContent.characterImage}
+              icon={popupContent.icon}
+              onClose={popupContent.onClose}
+            />
+          ) : (
+            <MissionPopup
+              congratsMessage={popupContent.congratsMessage}
+              storyline={popupContent.storyline}
+              description={popupContent.description}
+              name={popupContent.name}
+              characterImage={popupContent.characterImage}
+              icon={popupContent.icon}
+              onClose={popupContent.onClose}
+            />
+          )}
         </div>
       )}
     </>
