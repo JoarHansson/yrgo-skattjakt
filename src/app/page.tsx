a06ee3ee14e9cde37071b70a2e7d9fd9b2d231db
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-08-22 17:20:42 +0200

    update css for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,35 @@
 import Link from "next/link";
-import TowerShipPirate from "@/content/tower-ship-pirate.png";
+import Roger from "@/content/roger-firstpage.png";
 import Sword from "@/content/sword.png";
 import ScrollBig from "@/content/scroll-big.png";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen relative">
-      <div className="p-4">
-        <img
-          src={TowerShipPirate.src}
-          alt="pirate and ship"
-          className="mx-auto max-w-xs "
-        />
-        <img
-          src={ScrollBig.src}
-          alt="scroll"
-          className="mx-auto max-w-sm px-8 "
-        />
-        <div className="w-[292px] pb-16 bg-cardDarker mx-auto -my-12 rounded-lg pt-16 px-4">
-          <h1 className="m-auto text-center heading">Skattjakten</h1>
-          <h1 className="m-auto text-center">på</h1>
-          <h1 className="m-auto text-center">Lindholmen</h1>
+    <main className="w-full h-screen relative bg-black">
+      <div className="flex justify-between flex-col h-full">
+        <div className="p-4">
+          <img
+            src={Roger.src}
+            alt="pirate and ship"
+            className="mx-auto p-8 pb-2"
+          />
+
+          <div className="text-[#FDF3E0]">
+            <h1 className="m-auto text-center heading ">Balderskägg</h1>
+            <p className="m-auto paragraph text-center">ett upptäckaräventyr</p>
+            <p className="m-auto paragraph text-center">på Lindholmen</p>
+          </div>
         </div>
-        <Link href={"/welcome"} className="absolute bottom-1 right-0">
-          <img src={Sword.src} alt="sword" />
-        </Link>
+
+        <div className="font-gelasio text-[14px] text-[#FDF3E0] text-center pb-8">
+          <p>av MF, UX och WU </p>
+          <p>® 2024</p>
+        </div>
       </div>
+      <Link
+        href={"/welcome"}
+        className="absolute top-0 left-0 h-full w-full z-20"
+      ></Link>
     </main>
   );
 }
